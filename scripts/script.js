8df1175ac9cafe0cec24d464cff89080b0b5e6b6
Hannah Olbrich <hannaholbrich@gmail.com>
2024-05-21 10:35:56 +0000

    Add hide and show functionality for food section on food, hotels and transport page

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Add in dropdown (hide and show functionality) for buttons on attracion page //
+// Add in dropdown (hide and show functionality) for buttons on attractions page //
 let attractionsButtons = document.getElementsByClassName("attractions-button")
 let attractionsDivs = document.getElementsByClassName("attractions-container")
 
@@ -8,6 +8,20 @@ for (let i = 0; i < attractionsButtons.length; i++) {
             attractionsDivs[i].style.display = "block";
         } else {
             attractionsDivs[i].style.display = "none";
+        }
+    })
+}
+
+// Add in dropdown (hide and show functionality) for buttons for the food and drinks sections on the attraction page //
+let foodDrinksButtons = document.getElementsByClassName("food-drinks-button")
+let foodDrinksDivs = document.getElementsByClassName("food-drinks-container")
+
+for (let i = 0; i < foodDrinksButtons.length; i++) {
+    foodDrinksButtons[i].addEventListener('click', function () {
+        if (foodDrinksDivs[i].style.display === "none") {
+            foodDrinksDivs[i].style.display = "block";
+        } else {
+            foodDrinksDivs[i].style.display = "none";
         }
     })
 }
