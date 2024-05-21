76c0318a2c2dbee5e682e2a030fb6a31054ff679
Hannah Olbrich <hannaholbrich@gmail.com>
2024-05-21 10:42:38 +0000

    Add hide and show functionality for rest of food, hotels, and transport page

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ for (let i = 0; i < attractionsButtons.length; i++) {
     })
 }
 
-// Add in dropdown (hide and show functionality) for buttons for the food and drinks sections on the attraction page //
+// Add in dropdown (hide and show functionality) for buttons for the food and drinks sections on the food, hotels and tranaport page //
 let foodDrinksButtons = document.getElementsByClassName("food-drinks-button")
 let foodDrinksDivs = document.getElementsByClassName("food-drinks-container")
 
@@ -22,6 +22,20 @@ for (let i = 0; i < foodDrinksButtons.length; i++) {
             foodDrinksDivs[i].style.display = "block";
         } else {
             foodDrinksDivs[i].style.display = "none";
+        }
+    })
+}
+
+// Add in dropdown (hide and show functionality) for buttons for hotels and transport sections on the food,hotels and transport page //
+let hotelsTransportButtons = document.getElementsByClassName("hotels-transport-button")
+let hotelsTransportDivs = document.getElementsByClassName("hotels-transport-container")
+
+for (let i = 0; i < hotelsTransportButtons.length; i++) {
+    hotelsTransportButtons[i].addEventListener('click', function () {
+        if (hotelsTransportDivs[i].style.display === "none") {
+            hotelsTransportDivs[i].style.display = "block";
+        } else {
+            hotelsTransportDivs[i].style.display = "none";
         }
     })
 }
