78231e25523803a7ccd6665fa2a9dbc9cb9f8ddb
Hannah Olbrich <hannaholbrich@gmail.com>
2024-06-13 13:23:51 +0000

    Move cards back to html again

--- assets/scripts/memory-game.js
+++ assets/scripts/memory-game.js
@@ -1,82 +1,5 @@
 // Memory Game
 
-// Cards Array
-document.addEventListener('DOMContentLoaded', () => {
-    let cardsArr = [{
-        name: "manc-city",
-        img: "assets/images/manc-city.png"
-    }, {
-        name: "manc-united",
-        img: "assets/images/manc-united.png"
-    }, {
-        name: "tram",
-        img: "assets/images/tram.png"
-    }, {
-        name: "market-stall",
-        img: "assets/images/market-stall.png"
-    }, {
-        name: "rain",
-        img: "assets/images/rain.png"
-    }, {
-        name: "restaurant",
-        img: "assets/images/restaurant.png"
-    }, {
-        name: "violin",
-        img: "assets/images/violin.png"
-    }, {
-        name: "park",
-        img: "assets/images/park.png"
-    }, {
-        name: "bee",
-        img: "assets/images/bee.png"
-    }, {
-        name: "art-gallery",
-        img: "assets/images/art-gallery.png"
-    }, {
-        name: "theatre",
-        img: "assets/images/theatre.png"
-    }, {
-        name: "river",
-        img: "assets/images/river.png"
-    }, {
-        name: "manc-city",
-        img: "assets/images/manc-city.png"
-    }, {
-        name: "manc-united",
-        img: "assets/images/manc-united.png"
-    }, {
-        name: "tram",
-        img: "assets/images/tram.png"
-    }, {
-        name: "market-stall",
-        img: "assets/images/market-stall.png"
-    }, {
-        name: "rain",
-        img: "assets/images/rain.png"
-    }, {
-        name: "restaurant",
-        img: "assets/images/restaurant.png"
-    }, {
-        name: "violin",
-        img: "assets/images/violin.png"
-    }, {
-        name: "park",
-        img: "assets/images/park.png"
-    }, {
-        name: "bee",
-        img: "assets/images/bee.png"
-    }, {
-        name: "art-gallery",
-        img: "assets/images/art-gallery.png"
-    }, {
-        name: "theatre",
-        img: "assets/images/theatre.png"
-    }, {
-        name: "river",
-        img: "assets/images/river.png"
-    }]
-})
-
 // Initialize classes annd ID's
 const cards = document.querySelectorAll(".card-item")
 const startButton = document.getElementById('start-button')
@@ -124,12 +47,12 @@ function startGame() {
 
 // Restart/reset game and play again
 function resetGame() {
-    shuffleCards();
-}
+    document.addEventListener("click", shuffleCards();
+    }
 
 
-// Timer in the game function
-// function startGameTimer() {}
+    // Timer in the game function
+    // function startGameTimer() {}
 
-// Ending game when timer is up
-// function endGame() {}
+    // Ending game when timer is up
+    // function endGame() {}
